3b946a02987dba7f0e67e5b69915c375c4851707
kidduts <dinuionutvladut99@gmail.com>
2021-03-26 17:09:37 +0200

    first commit

---
+++ src/Book.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let name;
+    export let age;
+    let clicks=0;
+    function funClick(){
+        clicks+=1;
+    }
+</script>
+
+<main>
+    <p>Hello {name}, you are {age} years old.</p>
+    <button on:click={funClick}>Clicked {clicks} {clicks===1 ? 'time' : 'times'}</button>
+</main>
